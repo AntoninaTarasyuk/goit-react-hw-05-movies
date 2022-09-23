d98a7f58decbac0a4be2104d4bc5f8dacb40a797
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2022-09-24 00:20:33 +0300

    change search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,11 @@
-import { useState, useEffect } from "react";
-import { useSearchParams } from 'react-router-dom';
+// import { useState, useEffect } from "react";
+// import { useSearchParams } from 'react-router-dom';
 import { FaSearch } from "react-icons/fa";
 
 import { Wrapper, SearchInput, SearchBtn } from "./SearchBar.styled";
 
-export const SearchBar = ({ value, onChange }) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [searchQuery, setSearchQuery] = useState(
-    () => searchParams.get("searchQuery") ?? ""
-  );
+export const SearchBar = () => {
 
-  useEffect(() => {
-    if (searchQuery === "") { return };
-  });
   return (
     <Wrapper>
       <form onSubmit={e => {
@@ -24,9 +17,9 @@ export const SearchBar = ({ value, onChange }) => {
           type="text"
           autoComplete="off"
           name="searchQuery"
-          value={searchQuery}
+          // value={searchQuery}
           placeholder="Enter movie name"
-          onChange={e => setSearchQuery(e.target.value)}
+          // onChange={e => setSearchQuery(e.target.value)}
         />
         <SearchBtn type="submit"><FaSearch /></SearchBtn>
       </form>
